e9d98d90aa67d54f3dd121ed44f4f88838819044
Vincent <vincent0740@gmail.com>
2018-01-02 03:01:52 +0800

    change arch

--- public/movie_intro.php
+++ public/movie_intro.php
@@ -1,2 +1,9 @@
-<?php require_once(__DIR__ . '/../layouts/header.php'); ?>
-<?php require_once(__DIR__ . '/../layouts/footer.php'); ?>
+<?php
+require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'environment.php');
+require_once(__DIR__ . '/../autoload.php');
+require_once(__DIR__ . '/../views/layouts/header.view.php');
+use \Data\DatabaseFactory;
+$db = new DatabaseFactory();
+require_once(__DIR__ . '/../views/movie_intro.view.php');
+
+require_once(__DIR__ . '/../views/layouts/footer.view.php');
---
+++ views/index.view.php
@@ -0,0 +1,154 @@
+<!-- https://coolors.co/e9eb87-e2dbbe-9dbbae-769fb6-188fa7
+https://coolors.co/e9eb87-241623-f05d5e-0f7173-d56f3e
+https://coolors.co/e9eb87-e2dbbe-7fb7be-d3f3ee-241623-->
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div id="my-carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#ad-carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#ad-carouse" data-slide-to="1"></li>
+                    <li data-target="#ad-carouse" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="d-block img-fluid" src="imgs/歌喉讚3-BIG.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid" src="imgs/與神同行-BIG.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid" src="imgs/大娛樂家-BIG.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#my-carousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#my-carousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row now-release justify-content-around">
+        <div class="col-12">
+            <div class="titlebar">
+                <div>現正熱映-></div>
+                <hr />
+            </div>
+            <div class="row">
+                <div class="col-4 d-flex flex-wrap justify-content-center">
+                    <a href="#"><img src="imgs/歌喉讚3-small.jpeg" /></a>
+                    <div class="media ">
+                        <img src="imgs/保護級.png" />
+                        <div class="media-body align-self-center movie-title">
+                            <a href="#">
+                                <div>歌喉讚3</div>
+                                <div>2017/12/27</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex flex-wrap justify-content-center">
+                    <a href="#"><img src="imgs/與神同行-small.jpeg" /></a>
+                    <div class="media">
+                        <img src="imgs/輔導12級.png" />
+                        <div class="media-body align-self-center movie-title">
+                            <a href="#">
+                                <div>與神同行</div>
+                                <div>2017/12/22</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 d-flex flex-wrap justify-content-center">
+                    <a href="#"><img src="imgs/大娛樂家-small.jpeg" /></a>
+                    <div class="media">
+                        <img src="imgs/普遍級.png" />
+                        <div class="media-body align-self-center movie-title">
+                            <a href="#">
+                                <div>大娛樂家</div>
+                                <div>2017/12/20</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-around">
+        <div class="col-4 future-release">
+            <div class="titlebar">
+                <div>即將上映-></div>
+                <hr />
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <div>
+                        <a href=""><img src="imgs/縮小人生-small.jpeg" /></a>
+                        <div class="movie-title">
+                            <a href="#">
+                                <div class="text-center">縮小人生</div>
+                                <div class="text-center">2018/01/19</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-7 motd">
+            <div class="titlebar">
+                <div>活動公告-></div>
+                <hr />
+            </div>
+            <div class="row">
+                <div class="col d-flex justify-content-center">
+                    <table>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/27</td>
+                            <td>你上車了嗎？來看電影送521號公車票！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/26</td>
+                            <td>玉山聯名卡加碼送！活動只到明年3/1！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/24</td>
+                            <td>聖誕慶祝！情侶雙人套票大優惠！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/17</td>
+                            <td>單身過聖誕不難過！看電影送香蕉！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/11</td>
+                            <td>來香蕉影城帶香蕉 及享電影票買一送一優惠！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/08</td>
+                            <td>歌喉讚最終力作!12/27 美聲女力 終極對決</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/12/01</td>
+                            <td>來香蕉影城刷國泰世華卡 即送香蕉一串！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/11/23</td>
+                            <td>香蕉老闆作功德！即日起所有票種調降10元！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/11/17</td>
+                            <td>香蕉影城11周年慶！來看電影就送香蕉！</td>
+                        </tr>
+                        <tr onclick="location.href='#'">
+                            <td>2017/11/10</td>
+                            <td>你最光棍！光棍節單人看電影只要111元！</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
