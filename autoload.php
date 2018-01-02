e6b9b84e89a1eed850076e838d1c896295b670b8
Vincent <vincent0740@gmail.com>
2018-01-02 11:16:56 +0800

    rename App -> app

--- autoload.php
+++ autoload.php
@@ -9,7 +9,7 @@ function autoload($className)
     if ($lastNsPos = strrpos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
-        $fileName  = 'App' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        $fileName  = 'app' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
     
